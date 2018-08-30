75deb958cbfe004ad5824c5a767977c6277c324a
goingtop <sebasipi501@gmail.com>
2018-08-30 17:22:26 -0500

    carousel

--- DemoPages/DemoMenuMasterDetail.cs
+++ DemoPages/DemoMenuMasterDetail.cs
@@ -3,8 +3,11 @@ namespace DemoPages
 {
     public class DemoMenuMasterDetail
     {
-        public DemoMenuMasterDetail()
-        {
+         public class MasterPageItem{
+                public string Titulo { get; set; }
+                public string Icono { get; set; }
+                public Type TipoPagina { get; set; }
+            }
         }
     }
-}
+
---
+++ DemoPages/ListadoPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DemoPages
+{
+    public partial class ListadoPage : ContentPage
+    {
+        public ListadoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
